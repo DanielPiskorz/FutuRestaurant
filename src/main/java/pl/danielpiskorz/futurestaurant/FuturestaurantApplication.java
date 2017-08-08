56ea117a63d1c86e0e110c5d4200185da7d0982b
DanielPiskorz <eel5426@gmail.com>
2017-08-08 15:28:07 +0200

    Spring boot starter project.

---
+++ src/main/java/pl/danielpiskorz/futurestaurant/FuturestaurantApplication.java
@@ -0,0 +1,12 @@
+package pl.danielpiskorz.futurestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuturestaurantApplication.class, args);
+	}
+}
---
+++ src/main/java/pl/danielpiskorz/futurestaurant/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.danielpiskorz.futurestaurant;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FuturestaurantApplication.class);
+	}
+
+}
